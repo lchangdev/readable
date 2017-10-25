5dbc5d62fa1e9c65951ff7f5170f72ec8629c095
Lawrence Chang <lchangdev@gmail.com>
2017-10-24 20:31:46 -0700

    feat(app): create not found page

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,9 @@ import PostFormPage from './PostFormPage';
 import PostPage from './PostPage';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Navbar from './Navbar';
+import NotFound from './NotFound';
 
 class App extends Component {
   render() {
@@ -14,11 +15,14 @@ class App extends Component {
       <div className="App row">
         <div className="col-sm-10 col-sm-offset-1">
           <Navbar />
-          <Route exact path="/" component={HomePage} />
-          <Route path="/category/:id" component={CategoryPage} />
-          <Route path="/post-form/:action" component={PostFormPage} />
-          <Route path="/post/:id" component={PostPage} />
-          <Route path="/comment-form/:action" component={CommentFormPage} />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/category/:id" component={CategoryPage} />
+            <Route path="/post-form/:action" component={PostFormPage} />
+            <Route path="/post/:id" component={PostPage} />
+            <Route path="/comment-form/:action" component={CommentFormPage} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
       </div>
     );
